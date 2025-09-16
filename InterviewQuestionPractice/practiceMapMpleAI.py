1efd0c79af1fbfa7614d0467679a1f6249af8a0f
vibhusharma101 <sharma.vibhu101@gmail.com>
2025-09-16 16:25:44 +0530

    practice question

--- InterviewQuestionPractice/practiceMapMpleAI.py
+++ InterviewQuestionPractice/practiceMapMpleAI.py
@@ -23,6 +23,22 @@ def practice(arr,n):
         n= n-2
     return ""
 
+def practiceSecondIteration(arr,n):
+    bdth = n
+    leadingSpaces = 0
+    while n>0:
+        for index in range(leadingSpaces):
+            print(" ",end="")
+        for index in range(n):
+            print("+",end="")
+        for index in range(leadingSpaces):
+            print(" ",end="")
+        print("")
+        n= n-2
+        leadingSpaces = leadingSpaces+2
+    return ""
+
 practice([],5)
+practiceSecondIteration([],3)
 
 ## Learn How Authentication Works and all.
